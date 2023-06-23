c3130419eb88b11b2352b25e3fe691edef989268
thibaut denis <thibaut_du_denis@hotmail.fr>
2023-06-23 15:32:02 +0200

    préparation mise en ligne

--- src/COMPONENTS/Carrousel.js
+++ src/COMPONENTS/Carrousel.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
@@ -7,14 +7,25 @@ import CloseIcon from '@mui/icons-material/Close';
 import data from '../DATA/Data';
 import '../CSS/Carrousel.css';
 //import { getProjects } from '../connection/Api';
-import Booki from '../IMAGES/projet_Booki.webp';
 
 SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 function Carousel() {
   //const [projects, setProjects] = useState([]);
   const [selectedItem, setSelectedItem] = useState(null);
+  const [isMobile, setIsMobile] = useState(false);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 767);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   // useEffect(() => {
   //   const fetchProjects = async () => {
@@ -33,60 +44,6 @@ function Carousel() {
     setSelectedItem(null);
   };
 
-//   const data = [
-//     {
-//     id : 1,
-//     name : 'Booki',
-//     image : "http://localhost:3000/static/media/projetBooki.0e8f64079d4f4be9b08a.webp",
-//     lien : 'https://github.com/ThibautTobi/Projet_2.git',
-//     langage : ['HTML', 'CSS'] ,
-//     description : `Projet OpenClassrooms : Créez la page d'accueil d'une agence de voyage Booki avec HTML & CSS,
-//     la page est en responsive pour écran d'ordinateur,tablette,mobile.`
-//     },
-//     {
-//     id : 2,
-//     name : 'Sophie Bluel',
-//     image : '../IMAGES/projet_Sophie_bluel.webp',
-//     lien : 'https://github.com/ThibautTobi/Projet_3_Portfolio-architecte-sophie-bluel.git',
-//     langage : ['HTML', 'CSS', 'JavaScript'] ,
-//     description : `Projet OpenClassrooms : Créez une page web dynamique pour le portfolio de Sophie Bluel avec JavaScript.`,
-//     },
-//     {
-//     id : 3,
-//     name : 'Qwenta',
-//     image : '../IMAGES/projet_qwenta.webp',
-//     lien : 'https://github.com/ThibautTobi?tab=repositories',
-//     langage : ['Gestion de Projet'] ,
-//     description : `Projet OpenClassrooms : Planifiez le développement du site de votre client Qwenta.`,
-//     },
-//     {
-//     id : 4,
-//     name : 'Nina Carducci',
-//     image : '../IMAGES/projet_Nina_Carducci.webp',
-//     lien : 'https://github.com/ThibautTobi/P5_Nina_Carducci.git',
-//     langage : ['HTML', 'CSS' ,'JavaScript'] ,
-//     description : `Projet OpenClassrooms : Débuggez et optimisez un site de photographe Nina Carducci.`,
-//     },
-//     {
-//     id : 5,
-//     name : 'Kasa',
-//     image : '../IMAGES/projet_Kasa.webp',
-//     lien : 'https://github.com/ThibautTobi/P6_Site_Kasa_Location_immobiliere.git',
-//     langage : ['HTML', 'CSS', 'JavaScript' ,'React'] ,
-//     description : `Projet OpenClassrooms : Créez une application web de location immobilière pour le client Kasa avec React.`,
-//     },
-//     {
-//     id : 6,
-//     name : 'Mon Vieux Grimoire',
-//     image : '../IMAGES/projet_Grimoire.webp',
-//     lien : 'https://github.com/ThibautTobi/P7-Back-End-Mon-Vieux-Grimoire.git',
-//     langage : ['Node Js', 'Express','mongoose','Mongo DB'] ,
-//     description : `Projet OpenClassrooms : Développez le back-end d'un site Mon Vieux Grimoire de notation de livres.`,
-//     },
-// ];
-
-
-
   return (
     <div>
       <Swiper
@@ -101,7 +58,7 @@ function Carousel() {
           <SwiperSlide key={item.id}>
             <div className="carousel_item" onClick={() => handleOpenModal(item)}>
               <div className="carousel_item_overlay" />
-              <img src={item.image} alt={item.name} className="carousel_item_img" />
+              <img src={isMobile ? item.imageMobile : item.image} alt={item.name} className="carousel_item_img" />
               <h3>{item.name}</h3>
               <a href={item.lien}>Lien vers le projet</a>
               <ul>
@@ -133,7 +90,7 @@ function Carousel() {
               <CloseIcon />
             </IconButton>
             <h2>{selectedItem?.name}</h2>
-            <img src={selectedItem?.image} alt={selectedItem?.name}/>
+            <img src={isMobile ? selectedItem?.imageMobile : selectedItem?.image} alt={selectedItem?.name}/>
             <a href={selectedItem?.lien}>Lien vers le projet</a>
             <ul>
               {selectedItem?.langage.map((lang) => (
@@ -144,7 +101,6 @@ function Carousel() {
           </div>
         </Fade>
       </Modal>
-      <img src={Booki} alt='booki'></img>
     </div>
   );
 }
--- src/DATA/Data.js
+++ src/DATA/Data.js
@@ -1,15 +1,22 @@
-import booki from '../IMAGES/projet_Booki.webp';
-import sophie from '../IMAGES/projet_Sophie_Bluel.webp';
-import qwenta from '../IMAGES/projet_qwenta.webp';
-import nina from '../IMAGES/projet_Nina_Carducci.webp';
-import kasa from '../IMAGES/projet_Kasa.webp';
-import grimoire from '../IMAGES/projet_Grimoire.webp';
+import booki from '../IMAGES/Booki.webp';
+import bookiMini from '../IMAGES/mini_Booki.webp';
+import sophie from '../IMAGES/Sophie_Bluel.webp';
+import sophieMini from '../IMAGES/mini_Sophie_Bluel.webp';
+import qwenta from '../IMAGES/qwenta.webp';
+import qwentaMini from '../IMAGES/mini_qwenta.webp';
+import nina from '../IMAGES/Nina_Carducci.webp';
+import ninaMini from '../IMAGES/mini_Nina_Carducci.webp';
+import kasa from '../IMAGES/Kasa.webp';
+import kasaMini from '../IMAGES/mini_Kasa.webp';
+import grimoire from '../IMAGES/Grimoire.webp';
+import grimoireMini from '../IMAGES/mini_Grimoire.webp';
 
 const data = [
     {
     id : 1,
     name : 'Booki',
     image : booki,
+    imageMobile: bookiMini,
     lien : 'https://github.com/ThibautTobi/Projet_2.git',
     langage : ['HTML', 'CSS'] ,
     description : `Projet OpenClassrooms : Créez la page d'accueil d'une agence de voyage Booki avec HTML & CSS,
@@ -19,6 +26,7 @@ const data = [
     id : 2,
     name : 'Sophie Bluel',
     image : sophie,
+    imageMobile: sophieMini,
     lien : 'https://github.com/ThibautTobi/Projet_3_Portfolio-architecte-sophie-bluel.git',
     langage : ['HTML', 'CSS', 'JavaScript'] ,
     description : `Projet OpenClassrooms : Créez une page web dynamique pour le portfolio de Sophie Bluel avec JavaScript.`,
@@ -27,6 +35,7 @@ const data = [
     id : 3,
     name : 'Qwenta',
     image : qwenta,
+    imageMobile: qwentaMini,
     lien : 'https://github.com/ThibautTobi?tab=repositories',
     langage : ['Gestion de Projet'] ,
     description : `Projet OpenClassrooms : Planifiez le développement du site de votre client Qwenta.`,
@@ -35,6 +44,7 @@ const data = [
     id : 4,
     name : 'Nina Carducci',
     image : nina,
+    imageMobile: ninaMini,
     lien : 'https://github.com/ThibautTobi/P5_Nina_Carducci.git',
     langage : ['HTML', 'CSS' ,'JavaScript'] ,
     description : `Projet OpenClassrooms : Débuggez et optimisez un site de photographe Nina Carducci.`,
@@ -43,6 +53,7 @@ const data = [
     id : 5,
     name : 'Kasa',
     image : kasa,
+    imageMobile: kasaMini,
     lien : 'https://github.com/ThibautTobi/P6_Site_Kasa_Location_immobiliere.git',
     langage : ['HTML', 'CSS', 'JavaScript' ,'React'] ,
     description : `Projet OpenClassrooms : Créez une application web de location immobilière pour le client Kasa avec React.`,
@@ -51,6 +62,7 @@ const data = [
     id : 6,
     name : 'Mon Vieux Grimoire',
     image : grimoire,
+    imageMobile: grimoireMini,
     lien : 'https://github.com/ThibautTobi/P7-Back-End-Mon-Vieux-Grimoire.git',
     langage : ['Node Js', 'Express','mongoose','Mongo DB'] ,
     description : `Projet OpenClassrooms : Développez le back-end d'un site Mon Vieux Grimoire de notation de livres.`,
