c92268d5af0b89527236e66946f21009bc644ecf
thibaut denis <thibaut_du_denis@hotmail.fr>
2023-06-26 16:29:33 +0200

    modification optimisation

--- Back-end/app.js
+++ Back-end/app.js
@@ -27,6 +27,9 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  // Définition des en-têtes de cache pour les ressources statiques
+  res.setHeader('Cache-Control', 'public, max-age=604800'); // 1 semaine
+
   next();
 });
 
